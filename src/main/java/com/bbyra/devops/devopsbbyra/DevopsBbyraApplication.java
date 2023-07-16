d986a82c37b0de713f0eb8696138cf9a5d539fbd
BastienBYRA <neitsabast@gmail.com>
2023-07-16 17:58:21 +0200

    Initial commit

---
+++ src/main/java/com/bbyra/devops/devopsbbyra/DevopsBbyraApplication.java
@@ -0,0 +1,13 @@
+package com.bbyra.devops.devopsbbyra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsBbyraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsBbyraApplication.class, args);
+    }
+
+}
